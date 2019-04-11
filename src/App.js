7ae76dee375d849eda9d70f2b10843e34b55c48e
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-04-11 16:36:44 -0700

    master overhaul 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Cart from './components/Cart'
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './theme';
 
 class App extends Component {
   render() {
     return (
        <BrowserRouter>
-            <div className="App">
-            
-              <Navbar/>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/cart" component={Cart}/>
-                  </Switch>
-             </div>
+          <MuiThemeProvider theme={theme}>
+          <div className="App">
+            <Navbar/>
+              <Switch>
+                  <Route exact path="/" component={Home}/>
+                  <Route path="/cart" component={Cart}/>
+                </Switch>
+           </div>
+          </MuiThemeProvider>
        </BrowserRouter>
       
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,138 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
- const Navbar = ()=>{
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import { InputBase } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+// import SearchIcon from '@material-ui/icons/Search'
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+      },
+      grow: {
+        flexGrow: 1,
+      },
+      menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+        color: '#ff9800',
+      },
+      title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+          display: 'block',
+        },
+      },
+      search: {
+        position: 'relative',
+        alignItems: 'left',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing.unit,
+          width: 'auto',
+        },
+      },
+    //   searchIcon: {
+    //     width: theme.spacing.unit * 9,
+    //     height: '100%',
+    //     position: 'absolute',
+    //     pointerEvents: 'none',
+    //     display: 'flex',
+    //     alignItems: 'center',
+    //     justifyContent: 'left',
+    //   },
+      actionFood: {
+        marginLeft: '1rem',
+      },
+      actionCart: {
+        marginLeft: '1rem',
+      },
+      ul: {
+        listStyle: 'none',
+        width: '50%',
+      },
+      link: {
+        color: '#ff9800',
+        textDecoration: 'none',
+      },
+      actLink: {
+        color: '#ff9800',
+        textDecoration: 'none',
+      },
+      inputRoot: {
+        color: 'secondary',
+        width: '100%',
+        justifyContent: 'center',
+      },
+      inputInput: {
+        justifyContent: 'center',
+        color: '#fff',
+        paddingTop: '5px',
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit * 10,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: 120,
+          '&:focus': {
+            width: 200,
+          },
+        },
+      },
+});
+
+ function Navbar(props) {
+    const { classes } = props;
     return(
-            <nav className="nav-wrapper">
-                <div className="container">
-                    <Link to="/" className="brand-logo">Fox Box</Link>
-                    
-                    <ul className="right">
-                        <li><Link to="/">Food</Link></li>
-                        <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
-                    </ul>
-                </div>
-            </nav>
-   
-        
-    )
+        <nav className={classes.root}>
+        <AppBar position="static">
+          <Toolbar className={classes.toolbar}>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+              <Link className={classes.link} to="/">Fox Box</Link>
+            </Typography>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              {/* <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div> */}
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+            </div>
+            <div className={classes.actionFood}>
+                <Link className={classes.actLink} to="/"><i className="material-icons">fastfood</i></Link>
+            </div>
+            <div className={classes.actionCart}>
+                <Link className={classes.actLink} to="/cart"><i className="material-icons">shopping_cart</i></Link>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </nav>
+    );
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navbar);
